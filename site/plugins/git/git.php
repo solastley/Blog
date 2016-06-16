91b9b012d8cea2253fbe9533dd297b6622e861e3
Solomon Astley <solastley@gmail.com>
2016-06-16 17:13:47 -0400

    automatic commit from updated panel

--- site/plugins/git/git.php
+++ site/plugins/git/git.php
@@ -2,7 +2,7 @@
 function git_update() {
     $index_dir = kirby()->roots()->index();
     chdir($index_dir);
-    
+
     shell_exec("git config user.email 'solastley@gmail.com'");
     shell_exec("git config user.name 'Solomon Astley'");
     shell_exec("git config push.default simple");
@@ -10,7 +10,6 @@ function git_update() {
     shell_exec("git commit -m 'automatic commit from updated panel'");
 
     $pull_message = shell_exec("git pull");
-    error_log($pull_message);
 
     exec("git push", $out, $status);
 
--- site/templates/gitwidget.php
+++ site/templates/gitwidget.php
@@ -130,21 +130,7 @@
             }
 
             // Execute the git commands and update the site variables
-            $index_dir = kirby()->roots()->index();
-            chdir($index_dir);
-            shell_exec("git config user.email 'solastley@gmail.com'");
-            shell_exec("git config user.name 'Solomon Astley'");
-            shell_exec("git config push.default simple");
-            shell_exec("git add -A");
-            shell_exec("git commit -m 'automatic commit from updated panel'");
-            $pull_message = shell_exec("git pull");
-            error_log($pull_message);
-            exec("git push", $out, $status);
-
-            site()->update(array(
-                'pull_message' => $pull_message,
-                'push_status' => $status
-            ));
+            git_update();
         }
     }
  ?>
