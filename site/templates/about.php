cfc80812c8eae69d7c034378750448d78439261a
Solomon Astley <solastley@gmail.com>
2016-10-20 03:56:53 +0000

    automatic commit from updated panel

--- site/templates/about.php
+++ site/templates/about.php
@@ -8,45 +8,28 @@
    <div class="container">
        <div class="row">
            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-               <p>I am a full-time student and part-time web developer. I study
-                   computer science at The University of Pittsburgh. In the summer
-                   of 2016, I will be working as a software engineering intern at
-                   MAYA Design, located in Pittsburgh, PA.</p>
-               <p>I specialize in full-stack development. I have a large amount
-                   of familiarity with various languages, frameworks, and
-                   libraries, such as:</p>
-               <ul class="skills">
-                   <li>Java</li>
-                   <li>JavaScript</li>
-                   <li>Python</li>
-                   <li>PHP</li>
-                   <li>HTML5</li>
-                   <li>CSS3</li>
-               </ul>
-               <ul class="skills">
-                   <li>MongoDB</li>
-                   <li>Express</li>
-                   <li>AnuglarJS</li>
-                   <li>Node.js</li>
-                   <li>Flask</li>
-                   <li>Postgresql</li>
-               </ul>
-               <ul class="skills">
-                   <li>Kirby CMS</li>
-                   <li>MySQL</li>
-                   <li>jQuery</li>
-                   <li>Bootstrap</li>
-                   <li>Jade</li>
-                   <li>Sass</li>
-               </ul>
-               <p>Additionally, I have experience using AWS, in particular EC2,
-                   S3, and Elastic Beanstalk.</p>
-               <p>In my free time, I also really enjoy travelling! I believe that
-                    experiencing other cultures and visiting new places is not
-                    only fun, but also incredibly important to view the world
-                    for what it truly is - an insanely complicated and diverse
-                    place.</p>
-           </div>
+          	<p>
+			As a computer scientist and full-stack developer, I consider
+			 myself incredibly fortunate to have worked personally with some
+			 of the most talented designers that I know. Working with the engineering
+			 team at MAYA Design has taught me so much about the importance of
+			 human-centered design in development, and I continue to apply this
+			 knowledge in all of my work.
+		</p>
+		<p>
+			I will primarily be using this blog to post about some of my web dev 
+			adventures and to make some (hopefully) useful tutorials. I don't expect 
+			that it will get much traffic, but if I can help or impress even one 
+			viewer then I would be happy. Please feel free to comment on anything you 
+			read, it would probably make my day.
+		</p>
+		<p>
+			This fall I will be a full-time computer science student at The University 
+			of Pittsburgh, but I will be actively seeking employment opportunities for 
+			the summer of 2017. To read about or view some of my work, please visit 
+			my <a href="http://www.solomonastley.com">personal website</a>.
+		</p>
+	   </div>
        </div>
    </div>
 
